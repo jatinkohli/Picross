c02ac6ae25f55b07b8bf578fc9569c3caefab017
20jatink <20jatink@students.harker.org>
2020-03-25 08:56:57 -0700

    Create Picross grid

---
+++ index.js
@@ -0,0 +1,23 @@
+var createGrid = function() {
+    const defaultLength = 10;
+    const defaultHeight = 10;
+
+    var length = defaultLength;
+    var height = defaultHeight;
+
+    var str = "<table id=\"grid\">";
+    
+    for (var row of Array(height).keys()) {
+        str += "<tr>";
+
+        for (var box of Array(length).keys()) {
+            str += "<td id=\"initialBox\"></td>";
+        }
+
+        str += "</tr>";
+    }
+
+    str += "</table>";
+
+    document.getElementById("grid").innerHTML = str;
+};
